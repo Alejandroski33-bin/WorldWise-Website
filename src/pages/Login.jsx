6179d7bd7316e9654dcf274fc1cfbd1df4b54fff
Alejandroski33-bin <alecafer99@gmail.com>
2026-05-13 14:27:15 -0600

    updated new password to avoid warnings

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -15,7 +15,7 @@ import Button from "../components/Button";
 export default function Login() {
   // PRE-FILL FOR DEV PURPOSES
   const [email, setEmail] = useState("jack@example.com");
-  const [password, setPassword] = useState("qwerty");
+  const [password, setPassword] = useState("qwertyuiop");
   const { login, isAuthenticated } = useAuth();
   const navigate = useNavigate();
 
